26fe45b71d1a5e5a57c0710ea175ba9ed96ccd97
Henry-Azer <henryazer@outlook.com>
2023-01-24 12:39:01 +0200

    implement facetcher backend

---
+++ src/main/java/com/henry/facetcherbackend/FacetcherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.henry.facetcherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacetcherBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacetcherBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/henry/facetcherbackend/FacetcherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.facetcherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacetcherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
