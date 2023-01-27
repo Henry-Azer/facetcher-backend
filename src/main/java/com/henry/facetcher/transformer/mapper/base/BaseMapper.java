5352b5f405b85d4f773bb48d5f56dd365fc0f669
Henry-Azer <henryazer@outlook.com>
2023-01-27 15:45:58 +0200

    implement base mapper and it's generic configuration

---
+++ src/main/java/com/henry/facetcher/transformer/mapper/base/BaseMapper.java
@@ -0,0 +1,18 @@
+package com.henry.facetcher.transformer.mapper.base;
+
+import com.henry.facetcher.dto.base.BaseDto;
+import com.henry.facetcher.entity.base.BaseEntity;
+import org.mapstruct.MappingTarget;
+
+/**
+ * @author Henry Azer
+ * @since 03/11/2022
+ */
+public interface BaseMapper<Entity extends BaseEntity, Dto extends BaseDto> {
+
+    Entity dtoToEntity(Dto dto);
+
+    Dto entityToDto(Entity entity);
+
+    void updateEntity(Dto dto, @MappingTarget Entity entity);
+}
