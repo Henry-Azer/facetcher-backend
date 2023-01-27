5011f98f4c58a4e07fd821cf59d4fb932aa834a0
Henry-Azer <henryazer@outlook.com>
2023-01-27 15:50:53 +0200

    implement refresh token service

---
+++ src/main/java/com/henry/facetcher/dao/RefreshTokenDao.java
@@ -0,0 +1,21 @@
+package com.henry.facetcher.dao;
+
+import com.henry.facetcher.entity.RefreshToken;
+
+import java.util.Optional;
+
+/**
+ * @author Henry Azer
+ * @since 05/11/2022
+ */
+public interface RefreshTokenDao {
+    Optional<RefreshToken> findRefreshTokenByEmail(String email);
+
+    Optional<RefreshToken> findRefreshTokenByRefreshToken(String refreshToken);
+
+    RefreshToken createRefreshToken(RefreshToken refreshToken);
+
+    RefreshToken updateRefreshToken(RefreshToken refreshToken);
+
+    Boolean deleteRefreshTokenByEmail(String email);
+}
