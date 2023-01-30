72eab467c7ecd45f900f4ccbafa7ab3d48796883
Henry-Azer <henryazer@outlook.com>
2023-01-30 20:45:12 +0200

    enable spring aspectJ auto proxy

--- src/main/java/com/henry/facetcher/FacetcherApplication.java
+++ src/main/java/com/henry/facetcher/FacetcherApplication.java
@@ -2,8 +2,10 @@ package com.henry.facetcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class FacetcherApplication {
 
     public static void main(String[] args) {
