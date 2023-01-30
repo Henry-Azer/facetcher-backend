f380e541a49f613bbe6e0ba84539b0596361533f
Henry-Azer <henryazer@outlook.com>
2023-01-30 20:44:06 +0200

    implement facetcher message constants interface

---
+++ src/main/java/com/henry/facetcher/constants/FacetcherMessageConstants.java
@@ -0,0 +1,7 @@
+package com.henry.facetcher.constants;
+
+public interface FacetcherMessageConstants {
+    String LOGGED_IN_ASPECT = " - Logged in successfully at: ";
+    String LOGGED_OUT_ASPECT = " - Logged out successfully at: ";
+
+}
