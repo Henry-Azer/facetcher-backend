7b7ac4ee1778e35839ce96b47a71b1a1cc2576a8
Henry-Azer <henryazer@outlook.com>
2023-01-31 12:32:32 +0200

    override user submission mapper functions

--- src/main/java/com/henry/facetcher/transformer/mapper/UserSubmissionMapper.java
+++ src/main/java/com/henry/facetcher/transformer/mapper/UserSubmissionMapper.java
@@ -1,11 +1,12 @@
 package com.henry.facetcher.transformer.mapper;
 
-import com.henry.facetcher.transformer.mapper.base.BaseMapper;
-import com.henry.facetcher.transformer.mapper.base.GenericMapperConfiguration;
 import com.henry.facetcher.dto.UserSubmissionDto;
 import com.henry.facetcher.model.UserSubmission;
+import com.henry.facetcher.transformer.mapper.base.BaseMapper;
+import com.henry.facetcher.transformer.mapper.base.GenericMapperConfiguration;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * @author Henry Azer
@@ -13,4 +14,16 @@ import org.mapstruct.Mapper;
  */
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR, config = GenericMapperConfiguration.class)
 public interface UserSubmissionMapper extends BaseMapper<UserSubmission, UserSubmissionDto> {
+
+    @Override
+    @Mapping(target = "userId", source = "user.id")
+    @Mapping(target = "inputImageId", source = "inputImage.id")
+    @Mapping(target = "outputImageId", source = "outputImage.id")
+    UserSubmissionDto entityToDto(UserSubmission entity);
+
+    @Override
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "inputImage", ignore = true)
+    @Mapping(target = "outputImage", ignore = true)
+    UserSubmission dtoToEntity(UserSubmissionDto dto);
 }
