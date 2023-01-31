84e8a38fbbd93169649bd39ef28384f02b7492f9
Henry-Azer <henryazer@outlook.com>
2023-01-31 12:30:03 +0200

    refactor cleaning authentication service aspect

--- src/main/java/com/henry/facetcher/aop/AuthenticationServiceAspect.java
+++ src/main/java/com/henry/facetcher/aop/AuthenticationServiceAspect.java
@@ -1,20 +1,22 @@
 package com.henry.facetcher.aop;
 
-import com.henry.facetcher.constants.FacetcherMessageConstants;
 import com.henry.facetcher.dto.UserDto;
 import com.henry.facetcher.dto.UserLogDto;
 import com.henry.facetcher.dto.base.response.AuthResponse;
 import com.henry.facetcher.enums.UserLogStatus;
-import com.henry.facetcher.manager.JWTAuthenticationManager;
 import com.henry.facetcher.security.jwt.JWTAuthenticationUtil;
 import com.henry.facetcher.service.UserLogService;
 import com.henry.facetcher.service.UserService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+
+import static com.henry.facetcher.constants.FacetcherMessageConstants.LOGGED_IN_ASPECT;
+import static com.henry.facetcher.constants.FacetcherMessageConstants.LOGGED_OUT_ASPECT;
 
 /**
  * @author Henry Azer
@@ -23,30 +25,23 @@ import java.time.LocalDateTime;
 @Slf4j
 @Aspect
 @Component
+@AllArgsConstructor
 public class AuthenticationServiceAspect {
     private final UserService userService;
     private final UserLogService userLogService;
     private final JWTAuthenticationUtil authenticationUtil;
-    private final JWTAuthenticationManager authenticationManager;
-
-    public AuthenticationServiceAspect(UserService userService, UserLogService userLogService, JWTAuthenticationUtil authenticationUtil, JWTAuthenticationManager authenticationManager) {
-        this.userService = userService;
-        this.userLogService = userLogService;
-        this.authenticationUtil = authenticationUtil;
-        this.authenticationManager = authenticationManager;
-    }
 
     @AfterReturning(pointcut = "execution(* com.henry.facetcher.manager.JWTAuthenticationManager.login(..))", returning = "authResponse")
     public void logAfterUserLogin(AuthResponse authResponse) {
         log.info("AuthenticationServiceAspect: logAfterUserLogin() called");
-        userLogService.create(constructUserLogDto(UserLogStatus.LOGIN, FacetcherMessageConstants.LOGGED_IN_ASPECT, userService.findUserByEmail(authenticationUtil.getAccessTokenUserEmail(authResponse.getAccessToken()))));
+        userLogService.create(constructUserLogDto(UserLogStatus.LOGIN, LOGGED_IN_ASPECT, userService.findUserByEmail(authenticationUtil.getAccessTokenUserEmail(authResponse.getAccessToken()))));
         log.info("AuthenticationServiceAspect: logAfterUserLogin() ended");
     }
 
     @AfterReturning(pointcut = "execution(* com.henry.facetcher.manager.JWTAuthenticationManager.logout(..))")
     public void logAfterUserLogout() {
         log.info("AuthenticationServiceAspect: logAfterUserLogout() called");
-        userLogService.create(constructUserLogDto(UserLogStatus.LOGOUT, FacetcherMessageConstants.LOGGED_OUT_ASPECT, userService.findUserByEmail(authenticationManager.getCurrentUserEmail())));
+        userLogService.create(constructUserLogDto(UserLogStatus.LOGOUT, LOGGED_OUT_ASPECT, userService.getCurrentUser()));
         log.info("AuthenticationServiceAspect: logAfterUserLogin() ended");
     }
 
@@ -56,7 +51,7 @@ public class AuthenticationServiceAspect {
         userLogDto.setUser(userDto);
         userLogDto.setLogStatus(logStatus);
         userLogDto.setLogDate(LocalDateTime.now());
-        userLogDto.setLogMessage(userLogDto.getUser().getEmail() + logMessage + userLogDto.getLogDate().toString());
+        userLogDto.setLogMessage(userLogDto.getUser().getEmail() + logMessage + userLogDto.getLogDate());
         return userLogDto;
     }
 }
