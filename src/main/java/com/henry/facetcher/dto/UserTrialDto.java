a3af6d52d9ba9004fcb92cdb6b5f0f78c1640e01
Henry-Azer <henryazer@outlook.com>
2023-01-31 11:25:25 +0200

    implement user trial entity

---
+++ src/main/java/com/henry/facetcher/dto/UserTrialDto.java
@@ -0,0 +1,29 @@
+package com.henry.facetcher.dto;
+
+import com.henry.facetcher.dto.base.BaseDto;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Henry Azer
+ * @since 31/01/2023
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class UserTrialDto extends BaseDto {
+    private Long id;
+    private UserDto user;
+    private Long userId;
+    private ImageDto inputImage;
+    private Long inputImageId;
+    private ImageDto outputImage;
+    private Long outputImageId;
+    private String title;
+    private String description;
+    private LocalDateTime trialDate;
+    private String trailMessage;
+}
---
+++ src/main/java/com/henry/facetcher/model/UserTrial.java
@@ -0,0 +1,52 @@
+package com.henry.facetcher.model;
+
+import com.henry.facetcher.model.base.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * @author Henry Azer
+ * @since 31/01/2023
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "user_trial", schema = "public")
+public class UserTrial extends BaseEntity {
+
+    @Id
+    @SequenceGenerator(name = "user_trial_id_sequence", sequenceName = "user_trial_id_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_trial_id_sequence")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "input_image_id", referencedColumnName = "id", nullable = false)
+    private Image inputImage;
+
+    @OneToOne
+    @JoinColumn(name = "output_image_id", referencedColumnName = "id", nullable = false)
+    private Image outputImage;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "trial_date")
+    private LocalDateTime trialDate;
+
+    @Column(name = "trial_message")
+    private String trailMessage;
+}
