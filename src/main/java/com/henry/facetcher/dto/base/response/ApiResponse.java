cf84380fcf50c20443ea2424773235db79657817
Henry-Azer <henryazer@outlook.com>
2023-01-31 11:18:04 +0200

    add @NoArgsConstructor to ApiResponse

--- src/main/java/com/henry/facetcher/dto/base/response/ApiResponse.java
+++ src/main/java/com/henry/facetcher/dto/base/response/ApiResponse.java
@@ -2,12 +2,14 @@ package com.henry.facetcher.dto.base.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Henry Azer
  * @since 03/11/2022
  */
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ApiResponse {
     private Boolean success;
