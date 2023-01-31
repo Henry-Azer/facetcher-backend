200296273cd0b416e2bd45a956490b27cc856569
Henry-Azer <henryazer@outlook.com>
2023-01-31 12:32:48 +0200

    implement counting user trials by title

--- src/main/java/com/henry/facetcher/dao/repo/UserTrialRepo.java
+++ src/main/java/com/henry/facetcher/dao/repo/UserTrialRepo.java
@@ -10,4 +10,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UserTrialRepo extends JpaRepository<UserTrial, Long> {
+    Long countAllByTitle(String title);
 }
