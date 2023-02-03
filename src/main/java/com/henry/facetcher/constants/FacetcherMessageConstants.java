bae47a0f800b4c5fbfb9a543660af278d23405e8
Henry-Azer <henryazer@outlook.com>
2023-02-03 21:57:10 +0200

    add to facetcher message constants

--- src/main/java/com/henry/facetcher/constants/FacetcherMessageConstants.java
+++ src/main/java/com/henry/facetcher/constants/FacetcherMessageConstants.java
@@ -5,4 +5,5 @@ public interface FacetcherMessageConstants {
     String LOGGED_OUT_ASPECT = " - Logout at: ";
     String TRIAL_MESSAGE = " - Trial at: ";
     String SUBMISSION_MESSAGE = " - Submit at: ";
+    String INPUT_IMAGE_PATH = "src/main/resources/fdl/image_input/";
 }
