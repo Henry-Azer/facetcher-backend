23de9b8c80d22fcbcbb8836ca1971597c7fff332
Henry-Azer <henryazer@outlook.com>
2023-02-14 17:30:45 +0200

    refactor user tail table columns table

--- src/main/resources/db/chengelog/20230131_user-trail-table.sql
+++ src/main/resources/db/chengelog/20230131_user-trail-table.sql
@@ -10,24 +10,29 @@ CREATE SEQUENCE public.user_trial_id_sequence
 
 CREATE TABLE public.user_trial
 (
-    id                BIGINT                 NOT NULL DEFAULT nextval('user_trial_id_sequence'::regclass),
-    user_id           BIGINT                 NOT NULL,
-    input_image_id    BIGINT                 NOT NULL,
-    output_image_id   BIGINT                 NOT NULL,
-    gender            CHARACTER VARYING(50)  NOT NULL,
-    title             CHARACTER VARYING(250) NOT NULL,
-    description       TEXT                   NOT NULL,
-    trial_date        TIMESTAMP              NOT NULL,
-    trial_message     CHARACTER VARYING(100) NOT NULL,
+    id                 BIGINT                 NOT NULL DEFAULT nextval('user_trial_id_sequence'::regclass),
+    user_id            BIGINT                 NOT NULL,
+    user_submission_id BIGINT                 NOT NULL,
+    input_image_id     BIGINT                 NOT NULL,
+    output_image_id    BIGINT                 NULL,
+    image_properties   CHARACTER VARYING(250) NOT NULL,
+    gender             CHARACTER VARYING(50)  NOT NULL,
+    title              CHARACTER VARYING(250) NOT NULL,
+    description        TEXT                   NOT NULL,
+    exception_occurred BOOLEAN                NOT NULL,
+    exception_message  TEXT                   NULL,
+    trial_date         TIMESTAMP              NOT NULL,
+    trial_message      CHARACTER VARYING(100) NOT NULL,
 
-    created_date      TIMESTAMP              NOT NULL,
-    modified_date     TIMESTAMP              NOT NULL,
-    created_by        CHARACTER VARYING(100) NOT NULL,
-    modified_by       CHARACTER VARYING(100) NOT NULL,
-    marked_as_deleted BOOLEAN                NOT NULL DEFAULT FALSE,
+    created_date       TIMESTAMP              NOT NULL,
+    modified_date      TIMESTAMP              NOT NULL,
+    created_by         CHARACTER VARYING(100) NOT NULL,
+    modified_by        CHARACTER VARYING(100) NOT NULL,
+    marked_as_deleted  BOOLEAN                NOT NULL DEFAULT FALSE,
 
     CONSTRAINT user_trial_pk PRIMARY KEY (id),
     CONSTRAINT user_trial_user_id_fk FOREIGN KEY (user_id) REFERENCES public.user (id),
+    CONSTRAINT user_trial_user_submission_id_fk FOREIGN KEY (user_submission_id) REFERENCES public.user_submission (id),
     CONSTRAINT user_trial_input_image_id_fk FOREIGN KEY (input_image_id) REFERENCES public.image (id),
     CONSTRAINT user_trial_output_image_id_fk FOREIGN KEY (output_image_id) REFERENCES public.image (id)
 
