5ceb6d4451a0bc56113fe89904a512bd64b305ca
Henry-Azer <henryazer@outlook.com>
2023-02-14 20:54:58 +0200

    add FDL constants to facetcher constants

--- src/main/java/com/henry/facetcher/constants/FacetcherConstants.java
+++ src/main/java/com/henry/facetcher/constants/FacetcherConstants.java
@@ -1,9 +1,13 @@
 package com.henry.facetcher.constants;
 
-public interface FacetcherMessageConstants {
+public interface FacetcherConstants {
     String LOGGED_IN_ASPECT = " - Login at: ";
     String LOGGED_OUT_ASPECT = " - Logout at: ";
     String TRIAL_MESSAGE = " - Trial at: ";
     String SUBMISSION_MESSAGE = " - Submit at: ";
+    String FDL_EXCEPTION = "FDL - Exception Occurred";
+    String FDL_PATH = "/src/main/resources/fdl/fdl_model.py";
+    String FDL_DIRECTORY_PATH = "/src/main/resources/fdl";
     String INPUT_IMAGE_PATH = "src/main/resources/fdl/image_input/";
+    String OUTPUT_IMAGE_PATH = "src/main/resources/fdl/image_output/";
 }
