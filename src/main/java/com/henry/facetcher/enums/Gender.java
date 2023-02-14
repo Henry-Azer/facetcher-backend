a2caf74aa65ec880ff9cfb5e312aa91586ea4a95
Henry-Azer <henryazer@outlook.com>
2023-02-14 21:00:03 +0200

    refactor user gender

--- src/main/java/com/henry/facetcher/enums/Gender.java
+++ src/main/java/com/henry/facetcher/enums/Gender.java
@@ -5,11 +5,21 @@ package com.henry.facetcher.enums;
  * @since 04/11/2022
  */
 public enum Gender {
-    MALE("MALE"), FEMALE("FEMALE");
+    MALE("MALE", 1), FEMALE("FEMALE", 0);
 
     private final String gender;
+    private final Integer value;
 
-    Gender(String gender) {
+    Gender(String gender, Integer value) {
         this.gender = gender;
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public String getGender() {
+        return gender;
     }
 }
