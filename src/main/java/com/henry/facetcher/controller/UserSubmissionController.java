224965c4b8a0a79a0d1da686b8f60123ac3e6696
Henry-Azer <henryazer@outlook.com>
2023-02-14 21:10:37 +0200

    implement user submission controller

--- src/main/java/com/henry/facetcher/controller/UserSubmissionController.java
+++ src/main/java/com/henry/facetcher/controller/UserSubmissionController.java
@@ -1,6 +1,7 @@
 package com.henry.facetcher.controller;
 
 import com.henry.facetcher.controller.base.BaseController;
+import com.henry.facetcher.dto.UserSubmissionDto;
 import com.henry.facetcher.dto.base.response.ApiResponse;
 import com.henry.facetcher.service.UserSubmissionService;
 import lombok.AllArgsConstructor;
@@ -27,24 +28,56 @@ public class UserSubmissionController implements BaseController<UserSubmissionSe
         return userSubmissionService;
     }
 
-    @GetMapping("/find-all-user-submissions")
+    @GetMapping("/find-by-id/{userSubmissionId}")
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    public ApiResponse getUserSubmissionById(@PathVariable Long userSubmissionId) {
+        log.info("UserSubmissionController: getUserSubmissionById() called");
+        return new ApiResponse(true, LocalDateTime.now().toString(),
+                "User Submission fetched successfully.", getService().findById(userSubmissionId));
+    }
+
+    @GetMapping("/find-all")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ApiResponse findAllUserSubmissions() {
+        log.info("UserSubmissionController: findAllUserSubmissions() called");
+        return new ApiResponse(true, LocalDateTime.now().toString(),
+                "User Submissions fetched successfully.", getService().findAllUserSubmissions());
+    }
+
+    @GetMapping("/find-all-by-user-id/{userId}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ApiResponse findAllUserSubmissionsByUserId(@PathVariable Long userId) {
+        log.info("UserSubmissionController: findAllUserSubmissions() called");
+        return new ApiResponse(true, LocalDateTime.now().toString(),
+                "User Submissions  fetched successfully.", getService().findAllUserSubmissionsByUserId(userId));
+    }
+
+    @GetMapping("/current-user/find-all")
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     public ApiResponse findAllUserSubmissionsByCurrentUser() {
-        log.info("AuthenticationController: findAllUserSubmissionsByCurrentUser() called");
+        log.info("UserSubmissionController: findAllUserSubmissionsByCurrentUser() called");
         return new ApiResponse(true, LocalDateTime.now().toString(),
                 "User Submissions fetched successfully.", getService().findAllUserSubmissionsByCurrentUser());
     }
 
-    @PostMapping("/submit/{userTrialId}")
+    @GetMapping("/current-user/count")
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
-    public ApiResponse submitUserSubmissionByUserTrialId(@PathVariable Long userTrialId) {
-        log.info("UserSubmissionController: submitUserSubmissionByTrialId() called");
+    public ApiResponse getUserSubmissionsCountByCurrentUser() {
+        log.info("UserSubmissionController: getUserSubmissionsCountByCurrentUser() called");
         return new ApiResponse(true, LocalDateTime.now().toString(),
-                "User Submission submitted successfully.", getService().submitUserSubmissionByUserTrialId(userTrialId));
+                "User Submissions count fetched successfully.", getService().findUserSubmissionsCountByCurrentUser());
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    public ApiResponse createOrUpdateUserSubmission(@RequestBody UserSubmissionDto userSubmissionDto) {
+        log.info("UserSubmissionController: createOrUpdateUserSubmission() called");
+        return new ApiResponse(true, LocalDateTime.now().toString(),
+                "User Submission submitted successfully.", getService().createOrUpdate(userSubmissionDto));
     }
 
     @PutMapping("/{userSubmissionId}/toggle-deletion")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     public ApiResponse toggleUserSubmissionDeletionById(@PathVariable Long userSubmissionId) {
         log.info("UserSubmissionController: toggleUserSubmissionDeletionById() called");
         return new ApiResponse(true, LocalDateTime.now().toString(),
