2cbe08cfd22d86d4b55133c04cd15312b9bf20f1
Henry-Azer <henryazer@outlook.com>
2023-02-15 03:23:05 +0200

    implement user message controller

---
+++ src/main/java/com/henry/facetcher/controller/UserMessageController.java
@@ -0,0 +1,45 @@
+package com.henry.facetcher.controller;
+
+import com.henry.facetcher.controller.base.BaseController;
+import com.henry.facetcher.dto.UserMessageDto;
+import com.henry.facetcher.dto.base.response.ApiResponse;
+import com.henry.facetcher.service.UserMessageService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Henry Azer
+ * @since 15/02/2023
+ */
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/user-message")
+public class UserMessageController implements BaseController<UserMessageService> {
+    private final UserMessageService userMessageService;
+
+    @Override
+    public UserMessageService getService() {
+        return userMessageService;
+    }
+
+    @GetMapping("/find-all")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ApiResponse findAllUserMessages() {
+        log.info("UserMessageController: findAllUserMessages() called");
+        return new ApiResponse(true, LocalDateTime.now().toString(),
+                "User Messages fetched successfully.", getService().findAllUserMessages());
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    public ApiResponse createUserMessage(@RequestBody UserMessageDto userMessageDto) {
+        log.info("UserSubmissionController: createUserMessage() called");
+        return new ApiResponse(true, LocalDateTime.now().toString(),
+                "User Message created successfully.", getService().create(userMessageDto));
+    }
+}
