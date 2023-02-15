e100cbcc006a3859f5499f525131e99e7e76066d
Henry-Azer <henryazer@outlook.com>
2023-02-15 03:22:16 +0200

    rename column description to message

--- src/main/resources/db/chengelog/20230215_user-message-table.sql
+++ src/main/resources/db/chengelog/20230215_user-message-table.sql
@@ -10,16 +10,16 @@ CREATE SEQUENCE public.user_message_id_sequence
 
 CREATE TABLE public.user_message
 (
-    id                 BIGINT                 NOT NULL DEFAULT nextval('user_message_id_sequence'::regclass),
-    user_id            BIGINT                 NOT NULL,
-    title              CHARACTER VARYING(250) NOT NULL,
-    description        TEXT                   NOT NULL,
+    id                BIGINT                 NOT NULL DEFAULT nextval('user_message_id_sequence'::regclass),
+    user_id           BIGINT                 NOT NULL,
+    title             CHARACTER VARYING(250) NOT NULL,
+    message           TEXT                   NOT NULL,
 
-    created_date       TIMESTAMP              NOT NULL,
-    modified_date      TIMESTAMP              NOT NULL,
-    created_by         CHARACTER VARYING(100) NOT NULL,
-    modified_by        CHARACTER VARYING(100) NOT NULL,
-    marked_as_deleted  BOOLEAN                NOT NULL DEFAULT FALSE,
+    created_date      TIMESTAMP              NOT NULL,
+    modified_date     TIMESTAMP              NOT NULL,
+    created_by        CHARACTER VARYING(100) NOT NULL,
+    modified_by       CHARACTER VARYING(100) NOT NULL,
+    marked_as_deleted BOOLEAN                NOT NULL DEFAULT FALSE,
 
     CONSTRAINT user_message_pk PRIMARY KEY (id),
     CONSTRAINT user_message_user_id_fk FOREIGN KEY (user_id) REFERENCES public.user (id)
