57f68706f76d8042b31516df71aa067d851f47c7
Henry-Azer <henryazer@outlook.com>
2023-02-15 03:20:01 +0200

    refactor filter pagination request dto equality and hash code

--- src/main/java/com/henry/facetcher/dto/base/pagination/FilterPaginationRequest.java
+++ src/main/java/com/henry/facetcher/dto/base/pagination/FilterPaginationRequest.java
@@ -3,15 +3,17 @@ package com.henry.facetcher.dto.base.pagination;
 import com.henry.facetcher.dto.base.request.PaginationRequest;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 /**
  * @author Henry Azer
  * @since 03/11/2022
  */
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class FilterPaginationRequest<Filtration> extends PaginationRequest {
     private Filtration criteria;
     private Boolean deletedRecords;
