1853942d712c7b6b066afa6695e7eb86af403286
Henry-Azer <henryazer@outlook.com>
2023-03-09 16:24:11 +0200

    refactor number util, add logging

--- src/main/java/com/henry/facetcher/util/NumberUtil.java
+++ src/main/java/com/henry/facetcher/util/NumberUtil.java
@@ -1,6 +1,6 @@
 package com.henry.facetcher.util;
 
-import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Random;
 
@@ -8,15 +8,17 @@ import java.util.Random;
  * @author Henry Azer
  * @since 11/02/2023
  */
-@Component
+@Slf4j
 public class NumberUtil {
 
     public static double positiveRandomNumberUnderOne() {
+        log.info("NumberUtil: positiveRandomNumberUnderOne() called");
         Random random = new Random();
         return 0.3 + (0.5) * random.nextDouble();
     }
 
     public static int randomFiveDigits() {
+        log.info("NumberUtil: randomFiveDigits() called");
         Random random = new Random();
         return random.nextInt(100000) + 10000;
     }
