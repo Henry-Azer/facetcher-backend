1a3c1284c51e05f40e3a4a4a696dc2c22584abee
Henry-Azer <henryazer@outlook.com>
2023-03-09 16:34:08 +0200

    integrate fdl model with aws s3 storage cloud services

--- src/main/resources/fdl/fdl_model.py
+++ src/main/resources/fdl/fdl_model.py
@@ -3,6 +3,9 @@ import cv2
 import numpy as np
 import jittor as jt
 import sys
+import os
+import urllib.request
+from util.S3Uploader import S3Uploader
 
 jt.flags.use_cuda = 0
 print('FDL - Started.')
@@ -14,10 +17,11 @@ try:
     sys.argv = [sys.argv[0]]
     print('FDL - Properties: ', args_list)
 
-    image_path = './image_input/' + args_list[0]
-    print('FDL - Input File:', image_path)
+    req = urllib.request.urlopen(args_list[0])
+    arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+    print('FDL - Input File: ', args_list[0])
 
-    mat_img = cv2.imread(image_path)
+    mat_img = cv2.imdecode(arr, -1)
     mat_img = cv2.resize(mat_img, (512, 512), interpolation=cv2.INTER_CUBIC)
     mat_img = cv2.cvtColor(mat_img, cv2.COLOR_RGB2BGR)
     sketch = mat_img.astype(np.uint8)
@@ -31,10 +35,12 @@ try:
     combine_model.part_weight[''] = float(args_list[5])
     combine_model.predict_shadow(mat_img)
 
-    output_file = './image_output/' + args_list[0]
-    cv2.imwrite(output_file, cv2.cvtColor(combine_model.generated, cv2.COLOR_BGR2RGB))
+    generated_image = cv2.cvtColor(combine_model.generated, cv2.COLOR_BGR2RGB)
+    image_bytes = cv2.imencode('.jpg', generated_image)[1].tobytes()
+
+    s3_url = S3Uploader.upload_file(image_bytes, os.path.basename(args_list[0]))
+    print('FDL - Output File: ', s3_url)
     jt.gc()
-    print('FDL - Output File:', output_file)
 
 except Exception as exception:
     print('FDL - Exception Occurred')
