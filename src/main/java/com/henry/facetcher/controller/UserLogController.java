9538b81e0831b214b3a7d0228c39c3712ee8c733
Henry-Azer <henryazer@outlook.com>
2023-04-02 21:12:22 +0200

    implement find all users logs

--- src/main/java/com/henry/facetcher/controller/UserLogController.java
+++ src/main/java/com/henry/facetcher/controller/UserLogController.java
@@ -1,15 +1,24 @@
 package com.henry.facetcher.controller;
 
 import com.henry.facetcher.controller.base.BaseController;
+import com.henry.facetcher.dto.base.response.ApiResponse;
 import com.henry.facetcher.service.UserLogService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 /**
  * @author Henry Azer
  * @since 30/01/2023
  */
+@Slf4j
+@CrossOrigin
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/user-log")
@@ -19,5 +28,13 @@ public class UserLogController implements BaseController<UserLogService> {
     @Override
     public UserLogService getService() {
         return userLogService;
+    }
+
+    @GetMapping("/find-all")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    public ApiResponse findAllUserLogs() {
+        log.info("UserLogController: findAllUserLogs() called");
+        return new ApiResponse(true, LocalDateTime.now().toString(),
+                "Users Logs fetched successfully.", getService().findAll());
     }
 }
